bf4595876a49c10c2c2b74e0f77aac673b7c442a
Michal Durdina <michal.durdina@visualdna.com>
2015-03-19 09:30:03 +0100

    Making it nicer with explicit interruption checking and gracefull shutdown

--- concurrency-1/src/com/durdina/samples/threadinterruption/ThreadInterruption.java
+++ concurrency-1/src/com/durdina/samples/threadinterruption/ThreadInterruption.java
@@ -1,33 +1,84 @@
 package com.durdina.samples.threadinterruption;
 
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
+/**
+ * Example for thread interruption, graceful thread shutdown and resource cleanup.
+ *
+ * @author Michal Durdina
+ */
 public class ThreadInterruption {
 
-    private static final int NUM = 10;
-
+    // expensive resources that need proper cleanup on shutdown
+    private static final int NUM = 100000;
     private long[] data;
+    private DataPrinter dataPrinter = new DataPrinter();
 
-    private Runnable dataPrinter = new Runnable() {
+    public static void main(String[] args) {
+        ThreadInterruption tt = new ThreadInterruption();
+        try {
+            tt.start();
+            tt.printStatistics();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-        Random rand = new Random();
+    public void start() throws InterruptedException {
+        Thread dataPrinterThread = new Thread(dataPrinter);
+        dataPrinterThread.start();
+        // let's interrupt it after 500ms
+        Thread.sleep(500);
+        dataPrinterThread.interrupt();
+        dataPrinter.awaitCompletion();
+    }
+
+    public void printStatistics() {
+        System.out.println("Status of resources " + data.length);
+    }
+
+    class DataPrinter implements Runnable {
+
+        private Random rand = new Random();
+
+        private CountDownLatch executionComplete = new CountDownLatch(1);
+
+//        private Object executionComplete = new Object();
 
         @Override
         public void run() {
+            long start = System.currentTimeMillis();
+            System.out.println("starting the thread");
+
             data = randomFill(new long[NUM]);
 
             try {
                 int c = -1;
-                while (++c < data.length) {
+                while (++c < data.length && !Thread.currentThread().isInterrupted()) {
                     System.out.println(c + ": " + data[c]);
-                    Thread.sleep(1000L);
                 }
-            } catch (InterruptedException e) {
-                System.err.println("Interrupted");
             } finally {
                 System.out.println("releasing resources");
                 data = new long[1];
                 System.out.println("resources released");
+                executionComplete.countDown();
+//                synchronized (executionComplete) {
+//                    executionComplete.notify();
+//                }
+                System.out.println("Thread finishing - time elapsed: " + (System.currentTimeMillis() - start) / 1000F + " s");
+            }
+        }
+
+        public void awaitCompletion() {
+            try {
+                executionComplete.await();
+//                synchronized(executionComplete) {
+//                    executionComplete.wait();
+//                }
+                System.out.println("Shutdown complete");
+            } catch (InterruptedException e) {
+                System.out.println("Interrupt during shutdown: " + e.getMessage());
             }
         }
 
@@ -38,29 +89,5 @@ public class ThreadInterruption {
             return anArray;
         }
 
-    };
-
-    public static void main(String[] args) {
-        ThreadInterruption tt = new ThreadInterruption();
-        tt.run();
-        tt.printStatistics();
     }
-
-    public void run() {
-        Thread dataPrinterThread = new Thread(dataPrinter);
-        dataPrinterThread.start();
-        try {
-            dataPrinterThread.join(3000);
-            dataPrinterThread.interrupt();
-            dataPrinterThread.join();
-        } catch (InterruptedException e) {
-            // ignore
-            e.printStackTrace();
-        }
-    }
-
-    public void printStatistics() {
-        System.out.println("Status of resources " + data.length);
-    }
-
 }
