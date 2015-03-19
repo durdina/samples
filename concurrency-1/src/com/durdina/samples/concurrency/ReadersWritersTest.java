c7da76b215cdb81d3111862ac9528bb532f93c96
Michal Durdina <michal.durdina@visualdna.com>
2015-03-19 09:30:15 +0100

    formatting only

--- concurrency-1/src/com/durdina/samples/concurrency/ReadersWritersTest.java
+++ concurrency-1/src/com/durdina/samples/concurrency/ReadersWritersTest.java
@@ -1,4 +1,5 @@
 package com.durdina.samples.concurrency;
+
 import static org.junit.Assert.assertTrue;
 import java.util.concurrent.CountDownLatch;
 
@@ -9,58 +10,56 @@ import org.junit.Test;
 
 public class ReadersWritersTest {
 
-	@Test
-	public void test() throws Exception {
+    @Test
+    public void test() throws Exception {
 
-		Resource resource = new Resource();
-		ExecutorService pool = Executors.newFixedThreadPool(50);
-		
-		int N = 1000;
-		CountDownLatch doneSignal = new CountDownLatch(N);
-		
-		for (int i = 0; i < N; i++) {
-			pool.execute(new Worker(resource, doneSignal));
-		}
-		
-		doneSignal.await();
-		
-		Integer[] all = resource.readAll();
-		System.out.println("Written " + all.length + " items");
-		
-		
-		
-		for (int i = 1; i < all.length; i++) {
-			assertTrue(all[i] > all[i-1]);
-		}
-		
-	}
+        Resource resource = new Resource();
+        ExecutorService pool = Executors.newFixedThreadPool(50);
 
-	static class Worker implements Runnable {
+        int N = 1000;
+        CountDownLatch doneSignal = new CountDownLatch(N);
 
-		private Resource resource;
-		private CountDownLatch doneSignal;
+        for (int i = 0; i < N; i++) {
+            pool.execute(new Worker(resource, doneSignal));
+        }
 
-		public Worker(Resource resource, CountDownLatch doneSignal) {
-			this.resource = resource;
-			this.doneSignal = doneSignal;
-		}
+        doneSignal.await();
 
-		@Override
-		public void run() {
+        Integer[] all = resource.readAll();
+        System.out.println("Written " + all.length + " items");
 
-			int l = resource.readHighest();
-			int l2 = resource.readHighest();
-			
-			if (l2 == l) {
-				System.out.print((l+1) +  ", ");
-				resource.write(l+1);
-			} else {
-				System.out.println("NOT INCREMENTING at " + l);
-			}
-			
-			doneSignal.countDown();
-			
-		}
-	};
+        for (int i = 1; i < all.length; i++) {
+            assertTrue(all[i] > all[i - 1]);
+        }
+
+    }
+
+    static class Worker implements Runnable {
+
+        private Resource resource;
+        private CountDownLatch doneSignal;
+
+        public Worker(Resource resource, CountDownLatch doneSignal) {
+            this.resource = resource;
+            this.doneSignal = doneSignal;
+        }
+
+        @Override
+        public void run() {
+
+            int l = resource.readHighest();
+            int l2 = resource.readHighest();
+
+            if (l2 == l) {
+                System.out.print((l + 1) + ", ");
+                resource.write(l + 1);
+            } else {
+                System.out.println("NOT INCREMENTING at " + l);
+            }
+
+            doneSignal.countDown();
+
+        }
+    };
 
 }
--- concurrency-1/src/com/durdina/samples/concurrency/Resource.java
+++ concurrency-1/src/com/durdina/samples/concurrency/Resource.java
@@ -1,34 +1,35 @@
 package com.durdina.samples.concurrency;
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class Resource {
 
-	List<Integer> nums = new ArrayList<Integer>();
+    List<Integer> nums = new ArrayList<Integer>();
 
-	public int readHighest() {
-		if (nums.size() > 0) {
-			return nums.get(nums.size() - 1);
-		} else {
-			return 0;
-		}
-	}
+    public int readHighest() {
+        if (nums.size() > 0) {
+            return nums.get(nums.size() - 1);
+        } else {
+            return 0;
+        }
+    }
 
-	public void write(int i) {
-		
-		synchronized (nums) {
-			if (nums.size() > 0) {
-				int last = nums.get(nums.size() - 1);
-				nums.add(last + 1);
-			} else {
-				nums.add(1);
-			}
-		}
-	}
+    public void write(int i) {
 
-	public Integer[] readAll() {
-		Integer[] a = new Integer[nums.size()];
-		return nums.toArray(a);
-	}
+        synchronized (nums) {
+            if (nums.size() > 0) {
+                int last = nums.get(nums.size() - 1);
+                nums.add(last + 1);
+            } else {
+                nums.add(1);
+            }
+        }
+    }
+
+    public Integer[] readAll() {
+        Integer[] a = new Integer[nums.size()];
+        return nums.toArray(a);
+    }
 
 }
