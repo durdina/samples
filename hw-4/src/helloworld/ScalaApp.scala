420da1ef637bd37026cef1d18dcf57f81f2944be
Michal Durdina <michal.durdina@visualdna.com>
2016-05-12 08:29:12 +0200

    my first hello world now formatted and cleaned up :)

--- hw-4/src/helloworld/ScalaApp.scala
+++ hw-4/src/helloworld/ScalaApp.scala
@@ -4,19 +4,17 @@ import java.text.DateFormat._
 import java.util.Date
 
 /**
- * Created by michaldurdina on 19/09/14.
- */
-object ScalaApp extends App {
+  * Created by michaldurdina on 19/09/14.
+  */
+object ScalaApp {
 
-  override def main(args: Array[String]) {
+  def main(args: Array[String]) {
     val now = new Date
     val df = getDateInstance
     println("Hi at " + (df format now))
 
     val timer = new Timer()
     timer.main()
-
-//    val tree = Var(1);
   }
 
 
--- hw-4/src/helloworld/Timer.scala
+++ hw-4/src/helloworld/Timer.scala
@@ -1,18 +1,20 @@
 package helloworld
 
 /**
- * Created by michaldurdina on 24/09/14.
- */
+  * Created by michaldurdina on 24/09/14.
+  */
 class Timer {
   def oncePerSecond(callback: () => Unit) {
     var i = 0
     while (i < 5) {
-      callback(); Thread sleep 1000; i = i + 1
+      callback();
+      Thread sleep 1000;
+      i = i + 1
     }
   }
 
   def main() {
-    oncePerSecond(() => println("time flies like an arrow..."))
+    oncePerSecond(() => println("time flies so fast..."))
   }
 
 }
