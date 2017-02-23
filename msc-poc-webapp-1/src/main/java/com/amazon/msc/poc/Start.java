90853a7b75f396ae570b53716388241b53461568
Michal Durdina <durdinam@amazon.com>
2017-02-23 06:19:05 -0800

    msc poc web app crafted partially by maven archetype and partially manually

---
+++ msc-poc-webapp-1/src/main/java/com/amazon/msc/poc/Start.java
@@ -0,0 +1,29 @@
+package com.amazon.msc.poc;
+
+import java.io.File;
+
+import org.apache.catalina.startup.Tomcat;
+
+/**
+ * Created by durdinam on 2017-02-23.
+ */
+public class Start {
+
+    public static void main(String[] args) throws Exception {
+
+        String webappDirLocation = "src/main/webapp/";
+        String tomcatWorkDir = "target/";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        System.out.println("configuring tomcat work dir: " +
+                new File("./" + tomcatWorkDir).getAbsolutePath());
+        tomcat.setBaseDir(tomcatWorkDir);
+        System.out.println("configuring app with basedir: " +
+                new File("./" + webappDirLocation).getAbsolutePath());
+        tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
