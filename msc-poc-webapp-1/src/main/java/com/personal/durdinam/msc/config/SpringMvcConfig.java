4ef19f34d24cf0e3265408a0da775d9b53f21d43
Michal Durdina <durdina@pobox.sk>
2024-03-21 13:20:08 +0100

    make it build again (still does not run though on Java 17 - minimal version required by Spring 6.0.0)

--- msc-poc-webapp-1/src/main/java/com/personal/durdinam/msc/config/SpringMvcConfig.java
+++ msc-poc-webapp-1/src/main/java/com/personal/durdinam/msc/config/SpringMvcConfig.java
@@ -6,9 +6,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -18,7 +17,7 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackageClasses = Start.class)
-public class SpringMvcConfig extends WebMvcConfigurerAdapter {
+public class SpringMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
